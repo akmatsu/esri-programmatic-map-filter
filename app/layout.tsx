e1083ad4bc007c639b36fd50d7083dc71190c8d7
f3ve <free.eve@pm.me>
2024-12-13 15:37:18 -0900

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,21 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: 'MSB Programmatic Map Viewer',
   description: 'A proof-of-concept for programmatically rendering and filtering GIS layers.',
+  openGraph: {
+    locale: 'en_US',
+    images: [
+      {
+        url: 'https://d1159zutbdy4l.cloudfront.net/public/uploads/9dad3c29-2e50-4d33-bf23-eed69a18abd8optimized_images/500x500_optimized_image.jpg',
+        width: 500,
+        height: 500,
+      },
+    ],
+  },
+  twitter: {
+    images: [
+      'https://d1159zutbdy4l.cloudfront.net/public/uploads/9dad3c29-2e50-4d33-bf23-eed69a18abd8optimized_images/500x500_optimized_image.jpg',
+    ],
+  },
 };
 
 export default function RootLayout({
